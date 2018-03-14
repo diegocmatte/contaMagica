b54f760983998fd142b926140e133d62092330d3
Diego Matte <diegocmatte@gmail.com>
2018-03-14 14:45:45 -0300

    criada classe com uso de big decimal

--- src/contamagica/Main.java
+++ src/contamagica/Main.java
@@ -18,21 +18,21 @@ public class Main {
         // TODO code application logic here
         System.out.println("--- Testando deposito ---");
         ContaMagica cm = new ContaMagica("joao");
-        cm.deposito(20000);
+        cm.setSaldo(20000);
         System.out.println("Saldo: "+cm.getSaldo()+" | Categoria: "+cm.getCategoria()); //silver
-        cm.setSaldo(100000);
         cm.deposito(100000);
-        cm.deposito(8000000);
+        cm.deposito(8000);
         System.out.println("Saldo: "+cm.getSaldo()+" | Categoria: "+cm.getCategoria()); //gold
         cm.deposito(200000);
         System.out.println("Saldo: "+cm.getSaldo()+" | Categoria: "+cm.getCategoria()); //platinum
         System.out.println("--- Fim teste deposito ---\n");
-       // System.out.println("--- Testando retirada ---");
-      //  ContaMagica cm1 = new ContaMagica("pedro");
-      //  cm1.deposito(200000);
-      //  System.out.println("Saldo: "+cm1.getSaldo()+" | Categoria: "+cm1.getCategoria()); //platinum
-      //  cm1.retirada(180000);
-      //  System.out.println("Saldo: "+cm1.getSaldo()+" | Categoria: "+cm1.getCategoria()); //gold
+        System.out.println("--- Testando retirada ---");
+        ContaMagica cm1 = new ContaMagica("pedro");
+        cm1.deposito(200000);
+        System.out.println("Saldo: "+cm1.getSaldo()+" | Categoria: "+cm1.getCategoria()); //platinum
+        cm1.retirada(180000);
+        System.out.println("Saldo: "+cm1.getSaldo()+" | Categoria: "+cm1.getCategoria()); //gold
+        System.out.println("--- Fim teste deposito ---\n");
         
     }
     
